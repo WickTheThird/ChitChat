ce08b0f0c30e29fa73ef87e1493babe84df1efd4
WickTheThird <filip.bumbu2@mail.dcu.ie>
2023-06-27 11:45:33 +0100

    routers in index.testing.js

--- chitfront/src/App.test.js
+++ chitfront/src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
   console.log('Rendering App component in test:', App);
-  render(<App />);
+  
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    );
+  
   const linkElement = screen.getByTestId('learn-react-link');
   expect(linkElement).toBeInTheDocument();
 });
